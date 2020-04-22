855c19402c23620461c0e1f67b3e7db18f40d95a
duce73 <53637682+duce73@users.noreply.github.com>
2020-04-22 21:47:38 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,118 +1,240 @@
 #include <stdio.h>
-#include <malloc.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 
-#define MAX_SIZE 100
-#define DELTM "\t\n\r\a"
+//内置函数的声明
+int my_shell_cd(char **args);
+int my_shell_help(char **args);
+int my_shell_exit(char **args);
+int my_shell_clr(char **args);
+int my_shell_echo(char **args);
+int my_shell_dir(char **args);
+int my_shell_pause(char **args);
+int my_shell_environ(char **args);
 
-int my_cd(char **args){
-    if(args[1] == NULL){
-        fprintf(stderr,"please input argument to \"cd\"\n");
-    } else{
-        if(chdir(args[1]) != 0){
-            fprintf(stderr,"my_shell1");
-        }
-    }
-    return 1;
-} 
+//使用函数体指针的写法，便于功能的扩充以及后续的维护
+char *builtin_str[] = {
+  	"cd",
+  	"help",
+  	"exit",
+	"clr",
+	"echo",
+	"dir",
+	"pause",
+	"environ"
+};
 
-int my_exec(char **args){
-	//null
-	 if(args[0] == NULL){
-        return 1;
-    }
-    //cd
-    if(strcmp(args[0],"cd") == 0){
-        return my_cd(args);
-    }
-    //exit
-    if(strcmp(args[0],"exit") == 0){
-        return 0;
-    }
+int (*builtin_func[]) (char **) = {
+  	&my_shell_cd,
+  	&my_shell_help,
+  	&my_shell_exit,
+	&my_shell_clr,
+	&my_shell_echo,
+	&my_shell_dir,
+	&my_shell_pause,
+	&my_shell_environ
+};
 
-	pid_t pid;
-    int flag;
+int my_shell_numbuiltin() {
+  	return sizeof(builtin_str) / sizeof(char *);
+}
 
-	pid = fork();
-  	if (pid == 0) {
-    // Child process
-    	if (execvp(args[0], args) == -1) {
-      		perror("my_shell2");
+//实现内置命令cd
+int my_shell_cd(char **args){
+  	if (args[1] == NULL) {
+    	fprintf(stderr,"my_shell wraning1: input argument to\"cd\"\n");
+  	} else {
+    	if (chdir(args[1]) != 0) {
+      		perror("my_shell");
     	}
+  	}
+  	return 1;
+}
+
+//实现help命令
+int my_shell_help(char **args){
+  	int i;
+  	printf("hello, this is my_shell power by DC:\n");
+  	printf("Input those builtin commands.\n");
+  	for (i = 0; i < my_shell_numbuiltin(); i++) {
+    	printf("  %s\n", builtin_str[i]);
+  	}
+  	return 1;
+}
+
+//exit
+int my_shell_exit(char **args){
+  	return 0;
+}
+
+//清屏
+int my_shell_clr(char **args){
+	printf("%s","\033[1H\033[2J");
+	return 1;
+}
+
+//字符回显
+int my_shell_echo(char **args){
+	int position = 1;
+
+	while(args[position] != NULL){
+		printf("%s ",args[position]);
+		position++;
+	}
+	printf("\n");
+	return 1;
+}
+
+//暂停功能
+int my_shell_pause(char **args){
+	char c;
+	c = getchar();
+	while((c != EOF) && c != '\n'){
+		c = getchar();
+	}
+	return 1;
+}
+
+//列出环境变量
+int my_shell_environ(char **args){
+	int i;
+	for (i = 0; i < my_shell_numbuiltin(); i++) {
+    	printf("  %s\n", builtin_str[i]);
+  	}
+  	return 1;
+}
+
+//实现外部命令，通过fork函数与exec函数的配合
+int my_shell_fun(char **args){
+  	int pid;
+  	int status;
+
+  	pid = fork();
+  	if (pid == 0) {
+    	//子进程
+    	if (execvp(args[0], args) == -1) {
+      		perror("my_shell warning2:");
+   	 	}
     	exit(EXIT_FAILURE);
   	} else if (pid < 0) {
-    	// Error forking 
-    	perror("my_shell3");
+    	//fork出错
+    	perror("my_shell warning3:");
   	} else {
-    	// Parent process
+    	//父进程等待子进程
     	do {
-      		waitpid(pid, &flag, WUNTRACED);
-    	} while (!WIFEXITED(flag) && !WIFSIGNALED(flag));
-  }
-  return 1;
+      		waitpid(pid, &status, WUNTRACED);
+    	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  	}
+  	return 1;
 }
 
-
-char **split_line(char *line){
-	int num = 0;
-	char **args = malloc(MAX_SIZE*sizeof(char*));
-	char *arg;
-	arg = strtok(line,DELTM);
-	while(arg != NULL){
-		args[num] = arg;
-		num++;
-		arg = strtok(NULL,DELTM);
-	}
-	args[num] = NULL;
-	return args;
+//列出当前目录下的文件与文件夹
+int my_shell_dir(char **args){
+	args[0] = "ls";
+	return my_shell_fun(args);
 }
 
-
-
-char *read_line(void){
-	int num = 0;
-	char *line=malloc(sizeof(char)*MAX_SIZE);
-	int value;
-
-	while(1){
-		value = getchar();
-
-		if(value == EOF){
-			exit(EXIT_FAILURE);
-		}
-		else if(value == '\n'){
-			line[num] = '\0';
-			return line;
-		}
-		else{
-			line[num] = value;
-		}
-		num++;
-	}
+//实现了内置命令与外部命令的结合，并且对于输入为空也做了处理
+int my_shell_exec(char **args){
+  	int i;
+  	if (args[0] == NULL) {
+    	return 1;
+  	}
+  	for (i = 0; i < my_shell_numbuiltin(); i++) {
+    	if (strcmp(args[0], builtin_str[i]) == 0) {
+      		return (*builtin_func[i])(args);
+    	}
+  	}
+  	return my_shell_fun(args);
 }
 
+//从标准输入中读取一行
+#define READ 1000
 
+char *my_shell_readline(void){
+  	int position = 0;
+ 	char *buffer = malloc(sizeof(char) * READ);
+  	int c;
+
+  	if (!buffer) {
+    	fprintf(stderr, "my_shell: allocation error\n");
+    	exit(EXIT_FAILURE);
+  	}
+
+  	while (1) {
+    	c = getchar();
+
+    	if (c == EOF) {
+      		exit(EXIT_SUCCESS);
+    	} else if (c == '\n') {
+      		buffer[position] = '\0';
+      		return buffer;
+    	} else {
+      		buffer[position] = c;
+    	}
+    	position++;
+  	}
+}
+
+//将读取的进行处理，通过strtok函数
+#define SPLIT 100
+#define DELIM " \t\r\n\a"
+
+char **my_shell_split(char *line){
+  	int position = 0;
+  	char **tokens = malloc(SPLIT * sizeof(char*));
+  	char *token;
+
+	//内存分配出错
+  	if (!tokens) {
+    	fprintf(stderr, "my_shell: allocation error\n");
+    	exit(EXIT_FAILURE);
+  	}
+
+  	token = strtok(line, DELIM);
+  	while (token != NULL) {
+    	tokens[position] = token;
+    	position++;
+    	token = strtok(NULL, DELIM);
+  	}
+  	tokens[position] = NULL;
+  	return tokens;
+}
+
+//获取当前的路径path
+#define SIZE 100
+
+char *getpwd(){
+	char *path;
+	path = (char*)malloc(SIZE);
+	getcwd(path,100);
+	return path;
+}
+
+//my_shell的主体循环
 void my_shell_loop(void){
-	char *line;
-	char **args;
-	int flag;
+  	char *line;
+  	char **args;
+	char *path;
+  	int status;
 
-	do{
-		printf("my_shell$:");
-		line = read_line();
-		args = split_line(line);
-		flag = my_exec(args);
+  	do {
+		path = getpwd();
+		printf("%s ",path);
+		printf("my_shell>>");
+    	line = my_shell_readline();
+    	args = my_shell_split(line);
+    	status = my_shell_exec(args);
 
-		free(line);
-		free(args);
-	}while(flag);
+    	free(line);
+    	free(args);
+  	} while (status);
 }
 
-int main(int argc, char *argv[]){
-
-	my_shell_loop();
-	return EXIT_SUCCESS;
+int main(int argc, char **argv){
+  	my_shell_loop();
+  	return EXIT_SUCCESS;
 }
